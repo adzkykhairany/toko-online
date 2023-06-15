f3bec864eafa9e74cf670fa6e999eb80f7b7732d
JusticioCaesario <justicio.caesario@mhs.unsoed.ac.id>
2023-06-16 01:23:10 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,21 @@ use App\Http\Controllers\ProductController;
 */
 
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+
 Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+
 Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+
 Route::get('/products/{id}', [ProductController::class, 'getAvailable'])->name('products.getAvailable');
+
 Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
+
 Route::delete('/products/{id}', [ProductController::class, 'delete'])->name('products.delete');
+
 Route::get('/products/available', [ProductController::class, 'getAvailable'])->name('products.getAvailable');
-Route::get('/products/unavailable', 'ProductController@getUnavailable')->name('products.getUnavailable');
+
+Route::get('/products/unavailable', [ProductController::class,'getUnavailable'])->name('products.getUnavailable');
+
 Route::put('/products/{id}/stock', [ProductController::class, 'updateStock'])->name('products.updateStock');
 
 ?>
