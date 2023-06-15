b22f407dc15798f0ef984ca57d18813a2a1bbbbe
JusticioCaesario <justicio.caesario@mhs.unsoed.ac.id>
2023-06-16 00:18:36 +0700

    Update create.blade.php

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,27 +1,90 @@
 <!-- resources/views/products/create.blade.php -->
 
-<h1>Tambah Produk Baru</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Produk Baru</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
 
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+        h1 {
+            color: #333;
+            text-align: center;
+        }
 
-<form action="{{ route('products.store') }}" method="POST">
-    @csrf
-    <label for="nama_produk">Nama Produk:</label>
-    <input type="text" name="nama_produk" id="nama_produk" required>
+        .alert {
+            background-color: #f8d7da;
+            color: #721c24;
+            padding: 10px;
+            margin-bottom: 20px;
+            border: 1px solid #f5c6cb;
+            border-radius: 4px;
+        }
 
-    <label for="harga">Harga:</label>
-    <input type="number" name="harga" id="harga" required min="0">
+        form {
+            max-width: 400px;
+            margin: 0 auto;
+        }
 
-    <label for="stok">Stok:</label>
-    <input type="number" name="stok" id="stok" required min="0">
+        label {
+            display: block;
+            margin-bottom: 10px;
+            font-weight: bold;
+        }
 
-    <button type="submit">Simpan</button>
-</form>
+        input[type="text"],
+        input[type="number"] {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 20px;
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        button[type="submit"] {
+            background-color: #007bff;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        button[type="submit"]:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+<body>
+    <h1>Tambah Produk Baru</h1>
+
+    @if ($errors->any())
+        <div class="alert">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('products.store') }}" method="POST">
+        @csrf
+        <label for="nama_produk">Nama Produk:</label>
+        <input type="text" name="nama_produk" id="nama_produk" required>
+
+        <label for="harga">Harga:</label>
+        <input type="number" name="harga" id="harga" required min="0">
+
+        <label for="stok">Stok:</label>
+        <input type="number" name="stok" id="stok" required min="0">
+
+        <button type="submit">Simpan</button>
+    </form>
+</body>
+</html>
